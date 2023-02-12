3d382ffc68a2938133ed876d6a7599d29223ab33
Olga <vilgudolga@gmail.com>
2023-02-12 16:40:12 +0100

    UP

--- src/components/notifications/Notifications.jsx
+++ src/components/notifications/Notifications.jsx
@@ -1,18 +1,16 @@
-import { Component } from "react";
-import PropTypes from "prop-types";
-import styles from "./Notifications.module.css";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Notifications.module.css';
 
-class Notification extends Component {  
-    render() {
-        const { message } = this.props;
-        return (
-            <p className={styles.paragraph}>{message}</p>
-        ); 
-    }
+class Notification extends Component {
+  render() {
+    const { message } = this.props;
+    return <p className={styles.paragraph}>{message}</p>;
+  }
 }
 
 Notification.propTypes = {
-    message: PropTypes.string,    
-}
+  message: PropTypes.string,
+};
 
 export default Notification;
